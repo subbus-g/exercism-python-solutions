8b183a7ba79dda967d882bbf80f24352ae091b9a
subbus <subbareddyg83@gmail.com>
2024-10-25 11:17:36 +0530

    simplify using conditional expression

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,13 +1,14 @@
 def steps(number):
-    if number<=0 :
-     # example when argument is zero or a negative integer
+    if number <= 0:
+        # example when argument is zero or a negative integer
         raise ValueError("Only positive integers are allowed")
     steps = int()
     while number != 1:
-        # if n is even
-        if not number % 2:
-            number /= 2
-        else:
-            number = 3 * number + 1
-        steps+=1
+        # # if n is even
+        # if not number % 2:
+        #     number /= 2
+        # else:
+        #     number = 3 * number + 1
+        number = number / 2 if number % 2 == 0 else 3 * number + 1
+        steps += 1
     return steps
