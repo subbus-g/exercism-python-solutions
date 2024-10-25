10fa6378a4d6d0f91cd8dec167aa364d98b26a96
subbus <subbareddyg83@gmail.com>
2024-10-25 07:40:29 +0530

    add solution

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number<=0 :
+     # example when argument is zero or a negative integer
+        raise ValueError("Only positive integers are allowed")
+    steps = int()
+    while number != 1:
+        # if n is even
+        if not number % 2:
+            number /= 2
+        else:
+            number = 3 * number + 1
+        steps+=1
+    return steps
