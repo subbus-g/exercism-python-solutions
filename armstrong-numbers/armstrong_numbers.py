f4ccde2f55b4182ef5b7a05d51772e5c5c212a51
subbus <subbareddyg83@gmail.com>
2025-04-18 12:35:37 +0530

    add 2nd solution without using string conversion

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,9 +1,20 @@
+import armstrong_numbers_test
+
+
 def is_armstrong_number(number):
-    s=str(number)
-    # l = len(str(number))
-    sum=0
-    for c in s:
-        sum += int(c) ** len(s)
+    # s=str(number)
+    # # l = len(str(number))
+    # sum=0
+    # for c in s:
+    #     sum += int(c) ** len(s)
+    # return sum == number
+    digits = []
+    number_copy = number
+    while(number_copy):
+        last_digit = number_copy % 10
+        digits.append(last_digit)
+        number_copy //= 10
+    sum =0
+    for digit in digits:
+        sum += digit ** len(digits)
     return sum == number
-    
-is_armstrong_number(153)
