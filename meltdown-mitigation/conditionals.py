d90596b775095ae3da799ace17364ba87363aa69
subbus <subbareddyg83@gmail.com>
2025-04-25 15:05:59 +0530

    add initial solution

--- meltdown-mitigation/conditionals.py
+++ meltdown-mitigation/conditionals.py
@@ -14,7 +14,14 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    pass
+    if (
+        temperature < 800
+        and neutrons_emitted > 500
+        and temperature * neutrons_emitted < 500000
+    ):
+        return True
+    else:
+        return False
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
@@ -36,8 +43,20 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     (generated power/ theoretical max power)*100
     where generated power = voltage * current
     """
+    generated_power = voltage * current
+    efficiency = (generated_power / theoretical_max_power) * 100
 
-    pass
+    efficiency_band = str()
+    if efficiency >= 80:
+        efficiency_band = "green"
+    elif efficiency >= 60 and efficiency < 80:
+        efficiency_band = "orange"
+    elif efficiency >= 30 and efficiency < 60:
+        efficiency_band = "red"
+    elif efficiency < 30:
+        efficiency_band = "black"
+
+    return efficiency_band
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
@@ -53,4 +72,13 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
 
-    pass
+    status_code = str()
+    product = temperature * neutrons_produced_per_second
+    if product < 0.9 * threshold:
+        status_code = "LOW"
+    elif abs(threshold - product) <= 0.1 * threshold:
+        status_code = "NORMAL"
+    else:
+        status_code = "DANGER"
+    
+    return status_code
