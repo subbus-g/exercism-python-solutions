ad32ff453c282d958e49af225927f2e0011d622b
subbus <subbareddyg83@gmail.com>
2025-05-04 12:58:06 +0530

    add solution

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,7 @@
 def leap_year(year):
-    pass
+    # if the year is a century year
+    if year % 100 == 0:
+        return year % 400 == 0
+    # if the year is not a century year
+    else:
+        return year % 4 == 0
