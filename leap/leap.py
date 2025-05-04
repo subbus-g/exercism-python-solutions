831be1e4fb52776979227e68c4457c6a1a29f02a
subbus <subbareddyg83@gmail.com>
2025-05-04 13:20:00 +0530

    add one solution

--- leap/leap.py
+++ leap/leap.py
@@ -1,7 +1,11 @@
 def leap_year(year):
-    # if the year is a century year
-    if year % 100 == 0:
-        return year % 400 == 0
-    # if the year is not a century year
-    else:
-        return year % 4 == 0
+    # # if the year is a century year
+    # if year % 100 == 0:
+    #     return year % 400 == 0
+    # # if the year is not a century year
+    # else:
+    #     return year % 4 == 0
+
+    # year must be divisble by 4 and
+    # year must not be a century unless it is a centory
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
