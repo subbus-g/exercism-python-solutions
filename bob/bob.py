9b97a02d55bb8324b3e539cb0bae319329d817ab
subbus <subbareddyg83@gmail.com>
2025-05-08 13:15:20 +0530

    add solution

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,12 @@
-def response(hey_bob):
-    pass
+def response(hey_bob:str):
+    match hey_bob:
+        case _ if hey_bob.strip().endswith('?') and not hey_bob.isupper() :
+            return 'Sure.'
+        case _ if hey_bob.isupper() and not hey_bob.endswith('?'):
+            return "Whoa, chill out!"
+        case _ if hey_bob.isupper() and hey_bob.endswith('?'):
+            return "Calm down, I know what I'm doing!"
+        case _ if not hey_bob or hey_bob.isspace():
+            return "Fine. Be that way!"
+        case _ :
+            return "Whatever."
